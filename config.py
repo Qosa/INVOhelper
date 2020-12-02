ab33237126c1696af8a7d5fec8408dc51c7bf828
Łukasz Kołosiński <lukasz.kolosinski@INEA.AD>
2020-12-02 15:19:45 +0100

    Postgres for Heroku

--- config.py
+++ config.py
@@ -11,7 +11,10 @@ UPLOAD_PATH = 'uploads'
 
 TEMPLATES_AUTO_RELOAD = True
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/invohelper'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/invohelper'
+
+SQLALCHEMY_DATABASE_URI = 'postgres://rmuwodwxizvomv:68d9a2ed7cb3bdf90df6e084fb709a4468597c1a875b7b91917c4f2560758fdb@ec2-52-31-94-195.eu-west-1.compute.amazonaws.com:5432/d5tl0po5fk7fpd'
+
 
 SECRET_KEY = 'you-will-never-guess'
 
