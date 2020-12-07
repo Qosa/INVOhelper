04083d748e6f741036a4909c872018ca186aa793
Łukasz Kołosiński <lukasz.kolosinski@INEA.AD>
2020-12-07 22:14:01 +0100

    API post test 6

--- app/main/api/views.py
+++ app/main/api/views.py
@@ -36,7 +36,7 @@ def api_add_item(inv_id,inv_number):
 @api.route('/item/post/add', methods=['GET', 'POST'])
 def api_add_item():
     if request.method == 'POST':
-        print(request.data)
+        print(request.data["comment"])
         print('kokoko!')
         data = {'inv_id':1,'inv_item_number':'1000010000006','comment':'jakies gowno','inv_response':1}
     else:
